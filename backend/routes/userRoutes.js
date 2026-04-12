efad5cc27654c2efd8a6f057f6f3b62a6a190613
CY <cyrusjf2103@gmail.com>
2026-04-13 00:52:13 +0800

    test vercel route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,8 +5,8 @@ const { authMiddleware, roleMiddleware } = require('../middleware/auth');
 const router = express.Router();
 
 // Only Kaluppa Foundation and DTI can access user management
-// router.get('/', authMiddleware, roleMiddleware(['Kaluppa Foundation', 'DTI']), getAllUsers);
-router.get('/', getAllUsers);
+router.get('/', authMiddleware, roleMiddleware(['Kaluppa Foundation', 'DTI']), getAllUsers);
+// router.get('/', getAllUsers);
 router.post('/', authMiddleware, roleMiddleware(['Kaluppa Foundation', 'DTI']), createUser);
 router.patch('/:userId', authMiddleware, roleMiddleware(['Kaluppa Foundation', 'DTI']), updateUser);
 router.delete('/:userId', authMiddleware, roleMiddleware(['Kaluppa Foundation', 'DTI']), deleteUser);
