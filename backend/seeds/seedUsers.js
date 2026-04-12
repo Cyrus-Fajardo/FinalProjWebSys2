e0ee90d7393295f74c358377d3d372908fc636c6
CY <cyrusjf2103@gmail.com>
2026-04-13 02:08:59 +0800

    created seed for admins

---
+++ backend/seeds/seedUsers.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
+const User = require('../models/User');
+const connectToDatabase = require('../config/db');
+
+const seedUsers = async () => {
+    if (!process.env.ALLOW_SEEDING) {
+  console.log('❌ Seeding disabled');
+  process.exit();
+}
+  await connectToDatabase();
+
+  await User.deleteMany(); // optional (clears old users)
+
+  const password = await bcrypt.hash('password123', 10);
+
+  await User.insertMany([
+    {
+      email: 'kaluppa@test.com',
+      password,
+      role: 'Kaluppa Foundation'
+    },
+    {
+      email: 'dti@test.com',
+      password,
+      role: 'DTI'
+    }
+  ]);
+
+  console.log('Seed completed');
+  process.exit();
+};
+
+seedUsers();
